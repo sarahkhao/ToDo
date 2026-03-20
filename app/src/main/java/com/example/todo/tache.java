88beffedc7ebd66fd404219f0c3a955898d51d91
melanie552 <melanie.mathourais@gmail.com>
2026-03-20 09:03:04 +0100

    ajout constucteur

--- app/src/main/java/com/example/todo/tache.java
+++ app/src/main/java/com/example/todo/tache.java
@@ -1,4 +1,25 @@
 package com.example.todo;
 
+import java.util.Date;
+
 public class tache {
+
+    private String intitule ;
+    private String description;
+    private float duree;
+    private Date dateDebut;
+
+    private Date dateFin;
+    private int status;
+    private String contexte;
+
+    public tache (String intitule, String desc, float duree, Date dateDebut, Date dateFin, int status, String contexte){
+        this.intitule = intitule;
+        this.description = desc;
+        this.duree = duree;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.status = status;
+        this.contexte = contexte;
+    }
 }
