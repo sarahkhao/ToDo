d7892ea0a2d2b15de4cc306bdff097f18ab671b1
melanie552 <melanie.mathourais@gmail.com>
2026-03-20 10:14:35 +0100

    ajout methode

--- app/src/main/java/com/example/todo/Tache.java
+++ app/src/main/java/com/example/todo/Tache.java
@@ -13,6 +13,8 @@ public class Tache {
     private int status;
     private String contexte;
 
+    private int feal;
+
     public Tache(String intitule, String desc, float duree, Date dateDebut, Date dateFin, int status, String contexte){
         this.intitule = intitule;
         this.description = desc;
@@ -20,6 +22,72 @@ public class Tache {
         this.dateDebut = dateDebut;
         this.dateFin = dateFin;
         this.status = status;
+        this.contexte = contexte;
+    }
+
+    public void updateTache (String intitule, String desc, float duree, Date dateDebut, Date dateFin, int status, String contexte){
+        this.intitule = intitule;
+        this.description = desc;
+        this.duree = duree;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.status = status;
+        this.contexte = contexte;
+    }
+
+    public String getIntitule(){
+        return this.intitule;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public float getDuree(){
+        return this.duree;
+    }
+
+    public Date getDateDebut(){
+        return this.dateDebut;
+    }
+
+    public Date getDateFin(){
+        return this.dateFin;
+    }
+
+    public int getStatus(){
+        return this.status;
+    }
+
+    public String getContexte(){
+        return this.contexte;
+    }
+
+    public void setIntitule(String intitule){
+        this.intitule = intitule;
+    }
+
+    public void setDescription(String desc){
+        this.description = desc;
+    }
+
+    public void setDuree(float duree){
+        this.duree = duree;
+    }
+
+    public void setDateDebut(Date date){
+        this.dateDebut = date;
+    }
+
+    public void setDateFin(Date date){
+        this.dateFin = date;
+    }
+
+    public void setStatus(int stat){
+        this.status = stat;
+    }
+
+    public void setContexte(String contexte){
         this.contexte = contexte;
     }
 }
